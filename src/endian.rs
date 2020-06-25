ab4f7e91527a03377fe32a56ce8a3696d7a7b698
Maarten de Vries <maarten@de-vri.es>
2020-06-25 17:19:18 +0200

    Add unit tests for endian.rs

--- src/endian.rs
+++ src/endian.rs
@@ -23,3 +23,42 @@ pub fn read_u32_le(buffer: &[u8]) -> u32 {
 	let high = read_u16_le(&buffer[2..4]) as u32;
 	low | high << 16
 }
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	use assert2::assert;
+
+	#[test]
+	fn test_write_u16_le() {
+		let mut buffer = [0xFF; 4];
+		write_u16_le(&mut buffer[0..], 0x0000);
+		assert!(buffer == [0x00, 0x00, 0xFF, 0xFF]);
+
+		write_u16_le(&mut buffer[2..], 0x1234);
+		assert!(buffer == [0x00, 0x00, 0x34, 0x12]);
+	}
+
+	#[test]
+	fn test_read_u16_le() {
+		assert!(read_u16_le(&[0x00, 0x00, 0x34, 0x12]) == 0);
+		assert!(read_u16_le(&[0x34, 0x12]) == 0x1234);
+	}
+
+	#[test]
+	fn test_write_u32_le() {
+		let mut buffer = [0xFF; 8];
+		write_u32_le(&mut buffer[0..], 0x0000);
+		assert!(buffer == [0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF]);
+
+		write_u32_le(&mut buffer[4..], 0x12345678);
+		assert!(buffer == [0x00, 0x00, 0x00, 0x00, 0x78, 0x56, 0x34, 0x12]);
+	}
+
+
+	#[test]
+	fn test_read_u32_le() {
+		assert!(read_u16_le(&[0x00, 0x00, 0x00, 0x00, 0x78, 0x56, 0x34, 0x12]) == 0);
+		assert!(read_u16_le(&[0x78, 0x56, 0x34, 0x12]) == 0x12345678);
+	}
+}
