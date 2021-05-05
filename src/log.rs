6a7f0ddccce1aa73013154e59058cd708da9b5df
Maarten de Vries <maarten@de-vri.es>
2021-05-05 21:28:43 +0200

    Ensure compiler errors on invalid use of disabled log macros.

--- src/log.rs
+++ src/log.rs
@@ -3,23 +3,33 @@
 #[macro_use]
 mod details {
 	macro_rules! trace {
-		($($args:tt)*) => { ::log::trace!($($args)*) }
+		($($args:tt)*) => {{
+			::log::trace!($($args)*);
+		}}
 	}
 
 	macro_rules! debug {
-		($($args:tt)*) => { ::log::debug!($($args)*) }
+		($($args:tt)*) => {{
+			::log::debug!($($args)*);
+		}}
 	}
 
 	macro_rules! info {
-		($($args:tt)*) => { ::log::info!($($args)*) }
+		($($args:tt)*) => {{
+			::log::info!($($args)*);
+		}}
 	}
 
 	macro_rules! warn {
-		($($args:tt)*) => { ::log::warn!($($args)*) }
+		($($args:tt)*) => {{
+			::log::warn!($($args)*);
+		}}
 	}
 
 	macro_rules! error {
-		($($args:tt)*) => { ::log::error!($($args)*) }
+		($($args:tt)*) => {{
+			::log::error!($($args)*);
+		}}
 	}
 }
 
@@ -27,23 +37,36 @@ mod details {
 #[allow(unused)]
 #[macro_use]
 mod details {
+	// These macros all pass the arguments to `format_args!()`
+	// to trigger compilation failures even with the "log" feature disabled.
+
 	macro_rules! trace {
-		($($args:tt)*) => {};
+		($($args:tt)*) => {{
+			format_args!($($args)*);
+		}};
 	}
 
 	macro_rules! debug {
-		($($args:tt)*) => {};
+		($($args:tt)*) => {{
+			format_args!($($args)*);
+		}}
 	}
 
 	macro_rules! info {
-		($($args:tt)*) => {};
+		($($args:tt)*) => {{
+			format_args!($($args)*);
+		}}
 	}
 
 	macro_rules! warn {
-		($($args:tt)*) => {};
+		($($args:tt)*) => {{
+			format_args!($($args)*);
+		}}
 	}
 
 	macro_rules! error {
-		($($args:tt)*) => {};
+		($($args:tt)*) => {{
+			format_args!($($args)*);
+		}}
 	}
 }
