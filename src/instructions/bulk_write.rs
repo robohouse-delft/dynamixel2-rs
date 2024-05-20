20e552bfe8234e222687241fb3945e238eef067a
Maarten de Vries <maarten@de-vri.es>
2024-05-20 12:29:20 +0200

    Make `bulk_write()` take a reference that is `IntoIterator`.

--- src/instructions/bulk_write.rs
+++ src/instructions/bulk_write.rs
@@ -20,13 +20,44 @@ where
 	/// This function panics if the same motor ID is used for more than one write.
 	///
 	/// This function also panics if the data length for a motor exceeds the capacity of a `u16`.
-	pub fn bulk_write<Write, T>(&mut self, writes: &[Write]) -> Result<(), WriteError>
+	///
+	/// # Example
+	/// ```no_run
+	/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+	/// use dynamixel2::Bus;
+	/// use dynamixel2::instructions::BulkWriteData;
+	/// use std::time::Duration;
+	///
+	/// let mut bus = Bus::open("/dev/ttyUSB0", 57600, Duration::from_millis(20))?;
+	/// bus.bulk_write(&[
+	///   // Write a u32 value of 2000 to register 116 of motor 1.
+	///   BulkWriteData {
+	///     motor_id: 1,
+	///     address: 116,
+	///     data: 2000u32.to_le_bytes().as_slice(),
+	///   },
+	///   // Write a u16 value of 300 to register 102 of motor 2.
+	///   BulkWriteData {
+	///     motor_id: 2,
+	///     address: 102,
+	///     data: 300u16.to_le_bytes().as_slice(),
+	///   },
+	/// ])?;
+	/// # Ok(())
+	/// # }
+	/// ```
+	pub fn bulk_write<'a, I, T>(&mut self, writes: &'a I) -> Result<(), WriteError>
 	where
-		Write: std::borrow::Borrow<BulkWriteData<T>>,
+		&'a I: IntoIterator,
+		<&'a I as IntoIterator>::IntoIter: Clone,
+		<&'a I as IntoIterator>::Item: std::borrow::Borrow<BulkWriteData<T>>,
 		T: AsRef<[u8]>,
 	{
+		use std::borrow::Borrow;
+
+		let writes = writes.into_iter();
 		let mut parameter_count = 0;
-		for write in writes {
+		for write in writes.clone() {
 			let write = write.borrow();
 			let data = write.data.as_ref();
 			if data.len() > u16::MAX.into() {
