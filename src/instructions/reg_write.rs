ab375e685777f2482f78eab8d5f33bcd7d673dd6
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2025-01-13 23:32:46 +1300

    use Data trait with reg_write

--- src/instructions/reg_write.rs
+++ src/instructions/reg_write.rs
@@ -1,5 +1,6 @@
-use crate::bus::endian::{write_u16_le, write_u32_le};
+use crate::bus::endian::write_u16_le;
 use crate::{Client, Response, TransferError};
+use crate::bus::Data;
 use super::{instruction_id, read_response_if_not_broadcast};
 
 impl<SerialPort, Buffer> Client<SerialPort, Buffer>
@@ -16,7 +17,7 @@ where
 	///
 	/// You may specify [`crate::instructions::packet_id::BROADCAST`] as motor ID.
 	/// If you do, none of the devices will reply with a response, and this function will not wait for any.
-	pub fn reg_write(&mut self, motor_id: u8, address: u16, data: &[u8]) -> Result<Response<()>, TransferError<SerialPort::Error>> {
+	pub fn reg_write_bytes(&mut self, motor_id: u8, address: u16, data: &[u8]) -> Result<Response<()>, TransferError<SerialPort::Error>> {
 		self.write_instruction(motor_id, instruction_id::REG_WRITE, 2 + data.len(), |buffer| {
 			write_u16_le(&mut buffer[0..], address);
 			buffer[2..].copy_from_slice(data);
@@ -25,7 +26,7 @@ where
 		Ok(read_response_if_not_broadcast(self, motor_id)?)
 	}
 
-	/// Register a write command for a 8 bit value to a specific motor.
+	/// Register a write command for value to a specific motor.
 	///
 	/// Only one write command can be registered per motor.
 	///
@@ -34,47 +35,10 @@ where
 	///
 	/// You may specify [`crate::instructions::packet_id::BROADCAST`] as motor ID.
 	/// If you do, none of the devices will reply with a response, and this function will not wait for any.
-	pub fn reg_write_u8(&mut self, motor_id: u8, address: u16, value: u8) -> Result<Response<()>, TransferError<SerialPort::Error>> {
+	pub fn reg_write<T: Data>(&mut self, motor_id: u8, address: u16, value: T) -> Result<Response<()>, TransferError<SerialPort::Error>> {
 		self.write_instruction(motor_id, instruction_id::REG_WRITE, 2 + 1, |buffer| {
 			write_u16_le(&mut buffer[0..], address);
-			buffer[2] = value;
-			Ok(())
-		})?;
-		Ok(read_response_if_not_broadcast(self, motor_id)?)
-	}
-
-	/// Register a write command for a 16 bit value to a specific motor.
-	///
-	/// Only one write command can be registered per motor.
-	///
-	/// You can have all connected motors execute their registered write using [`Self::broadcast_action`],
-	/// or a single motor using [`Self::action`].
-	///
-	/// You may specify [`crate::instructions::packet_id::BROADCAST`] as motor ID.
-	/// If you do, none of the devices will reply with a response, and this function will not wait for any.
-	pub fn reg_write_u16(&mut self, motor_id: u8, address: u16, value: u16) -> Result<Response<()>, TransferError<SerialPort::Error>> {
-		self.write_instruction(motor_id, instruction_id::REG_WRITE, 2 + 2, |buffer| {
-			write_u16_le(&mut buffer[0..], address);
-			write_u16_le(&mut buffer[2..], value);
-			Ok(())
-		})?;
-		Ok(read_response_if_not_broadcast(self, motor_id)?)
-	}
-
-	/// Register a write command for a 32 bit value to a specific motor.
-	///
-	/// Only one write command can be registered per motor.
-	///
-	/// You can have all connected motors execute their registered write using [`Self::broadcast_action`],
-	/// or a single motor using [`Self::action`].
-	///
-	/// You may specify [`crate::instructions::packet_id::BROADCAST`] as motor ID.
-	/// If you do, none of the devices will reply with a response, and this function will not wait for any.
-	pub fn reg_write_u32(&mut self, motor_id: u8, address: u16, value: u32) -> Result<Response<()>, TransferError<SerialPort::Error>> {
-		self.write_instruction(motor_id, instruction_id::REG_WRITE, 2 + 4, |buffer| {
-			write_u16_le(&mut buffer[0..], address);
-			write_u32_le(&mut buffer[2..], value);
-			Ok(())
+			value.encode(&mut buffer[2..])
 		})?;
 		Ok(read_response_if_not_broadcast(self, motor_id)?)
 	}
