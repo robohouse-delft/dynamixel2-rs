20742df13926712b314f67e2446921524cf8e412
Maarten de Vries <maarten@de-vri.es>
2025-03-21 11:26:46 +0100

    Rename "integration_test" feature to "integration-tests".

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,8 +1,8 @@
 pub mod mock;
 pub mod real;
 
-#[cfg(not(feature = "integration_test"))]
+#[cfg(not(feature = "integration-tests"))]
 pub use mock::run_mock as run;
 
-#[cfg(feature = "integration_test")]
+#[cfg(feature = "integration-tests")]
 pub use real::run;
